2825986132ee2d682fab084cf259f68569b56815
Admin <smhemalathasm@gmail.com>
2024-12-20 08:22:29 +0530

    2nd

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
-  
+  region = "us-east-1"
+}
+
+variable "ami_value" {
+  description = "value"
+}
+
+variable "awsinstance_type" {
+  description = "value"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2"
+  ami = var.ami_value
+  instance_type = var.awsinstance_type
 }
